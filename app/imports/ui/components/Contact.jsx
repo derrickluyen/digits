986dba85c0671517f03cd7072d93aafe8eddfc49
Derrick Luyen <dluyen@hawaii.edu>
2019-03-11 12:49:07 -1000

    list contacts page mockup finished

---
+++ app/imports/ui/components/Contact.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Image, CardMeta, CardDescription } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Contact extends React.Component {
+  render() {
+    return (
+        <Card centered>
+          <CardContent>
+            <Image floated='right' size='mini' src={this.props.contact.image}></Image>
+            <CardHeader>{this.props.contact.firstName} {this.props.contact.lastName}</CardHeader>
+            <CardMeta>{this.props.contact.address}</CardMeta>
+            <CardDescription>{this.props.contact.description}</CardDescription>
+          </CardContent>
+        </Card>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(Contact);
