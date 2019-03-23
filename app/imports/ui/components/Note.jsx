50f7ed1567e2330c4dd8b7bf80e7ddfb9ec85b25
Derrick Luyen <dluyen@hawaii.edu>
2019-03-23 11:26:55 -1000

    notes-2 finished in 16 minutes.

---
+++ app/imports/ui/components/Note.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FeedEvent, FeedContent, FeedDate, FeedSummary} from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter, Link } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class Note extends React.Component {
+  render() {
+    return (
+        <FeedEvent>
+          <FeedContent>
+            <FeedDate content={this.props.note.createdAt.toLocaleDateString('en-US')} />
+            <FeedSummary>
+              {this.props.note.note}
+            </FeedSummary>
+          </FeedContent>
+        </FeedEvent>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+Note.propTypes = {
+  note: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(Note);
